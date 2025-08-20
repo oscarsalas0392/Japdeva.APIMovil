a108fb5b7d7730046956559bcf2bc3b7980db267
Oscar <oscar.salas03@gmail.com>
2025-08-20 07:42:00 -0600

    Ajustes Estandares

--- Japdeva.APIMovil.Usuarios/Controllers/WeatherForecastController.cs
+++ Japdeva.APIMovil.Usuarios/Controllers/WeatherForecastController.cs
@@ -1,35 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Japdeva.APIMovil.Usuarios.Controllers;
-
-[ApiController]
-[Route("api/weatherForecast")]
-public class WeatherForecastController : ControllerBase
+namespace Japdeva.APIMovil.Usuarios.Controller 
 {
-    const int WeatherForecastCount = 5;
-
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
+    [ApiController]
+    [Route("api/weatherForecast")]
+    public class WeatherForecastController : ControllerBase
     {
 
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        private static readonly string[] Summaries = new[]
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
+
+
--- Japdeva.APIMovil.Usuarios/WeatherForecast.cs
+++ Japdeva.APIMovil.Usuarios/WeatherForecast.cs
@@ -1,10 +1,10 @@
-    namespace Japdeva.APIMovil.Usuarios
+namespace Japdeva.APIMovil.Usuarios
+{
+    public class WeatherForecast
     {
-        public class WeatherForecast
-        {
-            public DateOnly Date { get; set; }
-            public int TemperatureC { get; set; }
-            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-            public string? Summary { get; set; }
+        public DateOnly Date { get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public string? Summary { get; set; }
     }
 }
